d5c711a03a9a15980eff6268d4f338c0a4c56b3d
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-04 21:10:38 -0600

    Agregando interfaz de chat

---
+++ Mensajes_Relevantes/Models/ChatSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mensajes_Relevantes.Models
+{
+    public class ChatSettings : IChatDatabaseSettings
+    {
+        public string MessagesCollectionName { get; set; }
+        public string UsersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface IChatDatabaseSettings
+    {
+        string MessagesCollectionName { get; set; }
+        string UsersCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
