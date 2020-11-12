d9a47e0403dac57153f9c6e3f34e98e3f8724060
EbGu3 <eb1993_jar@hotmail.com>
2020-11-12 03:44:02 -0600

    MVC

---
+++ MRDB/Models/MongoHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MRDB.Models
+{
+    public class MongoHelper
+    {
+        public static IMongoClient Client { get; set; }
+        public static IMongoDatabase Database { get; set; }
+        
+        public static string MongoConnection = "mongodb://127.0.0.1:27017";
+
+        public static string MongoDataBase = "MRDB";
+
+        internal static void ConnectToMongoService()
+        {
+            try
+            {
+                Client = new MongoClient(MongoConnection);
+                Database = Client.GetDatabase(MongoDataBase);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
--- Mensajes_Relevantes/Models/UserProperty.cs
+++ Mensajes_Relevantes/Models/UserProperty.cs
@@ -1,8 +1,13 @@
-﻿namespace Mensajes_Relevantes.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Mensajes_Relevantes.Models
 {
     public class UserProperty
     {
-        public string Id { get; set; }
+        [BsonId]
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public string User { get; set; }
         public string Password { get; set; }
