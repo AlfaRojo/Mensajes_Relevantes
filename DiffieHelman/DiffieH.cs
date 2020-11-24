5ed76e946b1eecb14ec4c2abc0ddc2b38d8f455f
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-23 19:05:13 -0600

    Implementando Diffie Hellman
    Biblioteca de clases

---
+++ DiffieHelman/DiffieH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace DiffieHelman
+{
+    public class DiffieH
+    {
+        public static System.Numerics.BigInteger DiffieHelmannAlgorithm(int numberA, int numberB)
+        {
+            Random rnd = new Random();
+            BigInteger numberG = rnd.Next(10, 31);
+            BigInteger numberP = 37;
+            BigInteger numberFromA = BigInteger.ModPow(numberG, (numberA), numberP);
+            BigInteger numberFromB = BigInteger.ModPow(numberG, (numberB), numberP);
+            BigInteger SecretKeyFromA = BigInteger.ModPow(numberFromB, (numberA), numberP);
+            BigInteger SecretKeyFromB = BigInteger.ModPow(numberFromA, (numberB), numberP);
+            if (SecretKeyFromA == SecretKeyFromB)
+            {
+                return SecretKeyFromA;
+            }
+            return 0;
+        }
+    }
+}
