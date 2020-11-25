6d888bdb8d10749896bb45eebb568561c8dcbaa7
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-24 20:12:34 -0600

    Agregando SDES en biblioteca y Consola

---
+++ Console_SDES/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SDES;
+
+namespace Console_SDES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\t..::SDES::..");
+                Console.WriteLine("Ingrese una opción");
+                Console.WriteLine("1) SDES");
+                Console.WriteLine("2) Salir");
+                var option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese texto");
+                        string encrypted_Text = encryptDecrypt.Encrypt(Console.ReadLine(), "0001100000");
+                        Console.WriteLine("Texto Cifrado: " + encrypted_Text);
+                        string decrypted_Text = encryptDecrypt.Decrypt(encrypted_Text, "0001100000");
+                        Console.WriteLine("Texto Descifrado: " + decrypted_Text);
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("Saliendo...");
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Ingese una opción válida.");
+                        break;
+                }
+            }
+        }
+    }
+}
