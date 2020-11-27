b000c971bf7698a56f0ad6095664e846a374df79
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-27 17:38:56 -0600

    Igualando DH code en usuarios

--- MRDB/Models/UserInformation.cs
+++ MRDB/Models/UserInformation.cs
@@ -56,19 +56,42 @@ namespace MRDB.Models
             var user = User1;
             if (user.Friends.Count > 0)
             {
-                user.Get_DH();
                 new_Contact.DH_Key = Convert.ToInt32(DiffieH.DiffieHelmannAlgorithm(User1.DH, User2.DH).ToString());
                 user.Friends.Add(new_Contact);
                 MongoHelper.Database.GetCollection<User>("User").FindOneAndReplace(x => (string)x.Nick_Name == ActualUser, user);
             }
             else
             {
-                user.Get_DH();
                 new_Contact.DH_Key = Convert.ToInt32(DiffieH.DiffieHelmannAlgorithm(User1.DH, User2.DH).ToString());
                 user.Friends.Add(new_Contact);
                 MongoHelper.Database.GetCollection<User>("User").FindOneAndReplace(x => (string)x.Nick_Name == ActualUser, user);
             }
+            Contact contact = new Contact { 
+                Nick_Name = User1.Nick_Name,
+                DH_Key = User1.DH
+            };
+            other_Contact(contact, User2.Nick_Name);
         }
+        private void other_Contact(Contact new_Contact, string ActualUser)
+        {
+            MongoHelper.ConnectToMongoService();
+            var User1 = MongoHelper.Database.GetCollection<User>("User").Find(d => d.Nick_Name == ActualUser).ToListAsync().Result[0];
+            var User2 = MongoHelper.Database.GetCollection<User>("User").Find(d => d.Nick_Name == new_Contact.Nick_Name).ToListAsync().Result[0];
+            var user = User1;
+            if (user.Friends.Count > 0)
+            {
+                new_Contact.DH_Key = User2.Friends[0].DH_Key;
+                user.Friends.Add(new_Contact);
+                MongoHelper.Database.GetCollection<User>("User").FindOneAndReplace(x => (string)x.Nick_Name == ActualUser, user);
+            }
+            else
+            {
+                new_Contact.DH_Key = User2.Friends[0].DH_Key;
+                user.Friends.Add(new_Contact);
+                MongoHelper.Database.GetCollection<User>("User").FindOneAndReplace(x => (string)x.Nick_Name == ActualUser, user);
+            }
+        }
+
         public UserInformation()
         {
             UserContacts = new List<Contact>();
