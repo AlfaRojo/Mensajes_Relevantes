9a4e8b04b6dcd3b12da234bd2ceac4ced53c4749
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-12-02 20:34:37 -0600

    Correcciones de variables en descarga

--- MRDB/Controllers/FileController.cs
+++ MRDB/Controllers/FileController.cs
@@ -7,13 +7,11 @@ namespace MRDB.Controllers
     public class FileController : Controller
     {
         // GET: FileController
-        public ActionResult FileDownload()
+        public ActionResult FileDownload(string emisor, string receptor, string file_Name)
         {
             ManagerFile managerFile = new ManagerFile();
-            var fileCont = managerFile.GetFileConten("EbGu3", "Ivannita", "prueba1.txt");
-            var OriginalnameFile = "prueba1.txt";
-            var NewNameFile = OriginalnameFile.Substring(0, OriginalnameFile.Length - 4);
-            var fileVirtualPath = $"{managerFile.FileAcction(NewNameFile, fileCont)}";
+            var fileCont = managerFile.GetFileConten(emisor, receptor, file_Name);
+            var fileVirtualPath = $"{managerFile.FileAcction(file_Name, fileCont)}";
             byte[] content = System.IO.File.ReadAllBytes(fileVirtualPath);
             var name = Path.GetFileName(fileVirtualPath);
             return File(content, System.Net.Mime.MediaTypeNames.Application.Octet, name);
